1cad0ee0a301185cd8a0714300c0e29a8b4c9e4a
smaru1111 <marusou20@gmail.com>
2023-06-07 16:54:25 +0900

    update swa startについて説明追加

--- api/HttpTrigger1/index.js
+++ api/HttpTrigger1/index.js
@@ -1,3 +1,6 @@
+// swa start --api-location ./api
+// でスタートして、localhost4280にアクセス
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
   
